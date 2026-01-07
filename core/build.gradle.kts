bd26e81b3cb2c86ed5fe96a74337d4e873e3d78c
1fexd <PGFvy85nC@protonmail.com>
2026-01-07 12:08:43 +0100

    chore: Remove unnecessary dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,15 +1,8 @@
 import fe.build.dependencies.Grrfe
-import fe.build.dependencies._1fexd
 
 plugins {
 }
 dependencies {
-    implementation(platform(Grrfe.std.bom))
-    implementation(Grrfe.std.core)
-    implementation(Grrfe.std.result.core)
-
-    implementation(platform(_1fexd.composeKit.bom))
-    implementation(_1fexd.composeKit.core)
     implementation(AndroidX.core.ktx)
 
     testImplementation(kotlin("test"))
